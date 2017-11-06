2628e39491236e28da4e4d7e82144747f72add34
JSai23 <23284633+JSai23@users.noreply.github.com>
2017-11-06 18:15:12 -0500

    Add files via upload

---
+++ ch2.hs
@@ -0,0 +1,27 @@
+-- Chapter 2 Questions
+-- Haskell is cruel 
+-- #killhaskell
+import Array
+
+--2.2b
+fact' 0 = 0
+fact' 1 = 1
+fact' n = n * fact' (n-1)
+
+--2.2c
+fact'' 0 = 0
+fact'' 1 = 1
+fact'' n = if n < 0 then -1
+	   else n * fact'' (n-1)
+
+--2.6a
+average [] = 0
+average numlist = sum(numlist)/fromIntegral(length(numlist))
+		  where
+			sum []= 0
+			sum (x:xs) = x + sum(xs)
+
+--2.8b
+rep 0 = [0]
+rep n = 
+
