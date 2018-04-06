9722e2325c4bc66c99ced16e92423a345fad13ba
JSai23 <23284633+JSai23@users.noreply.github.com>
2018-04-06 18:23:23 -0400

    Add files via upload

--- ch4.hs
+++ ch4.hs
@@ -1,1 +1,16 @@
+--4.1
+concat1 xs = foldr (++) [] xs
+concat2 xs = foldl (++) [] xs
+-- time efficiency in bigO
+-- O(n) 
+
+--4.2
+--original 
+comp f g l = [f x | x <- (map g l ) , x > 10] 
+-- new one pass (check with Dr.Ken) 
+comp' f g l = [x | x <- (f (map g l)) , x > 10]
+
+--4.3
+
+
 
