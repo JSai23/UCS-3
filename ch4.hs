58d395f6ae9c685f22ccaedf952fa62aafb18b8e
JSai23 <23284633+JSai23@users.noreply.github.com>
2018-04-23 18:31:30 -0400

    Add files via upload

--- ch4.hs
+++ ch4.hs
@@ -29,5 +29,33 @@ splithelp x l lessacc moreacc = if head(l) <= x
 		then splithelp x (tail(l)) ((head(l)) : lessacc) moreacc 
 		else splithelp x (tail(l)) lessacc ((head(l)) : moreacc)
 --tail recursive 
+-- Dr.ken said its not really recursive at all but it counts 
+split'' x l = ([y | y <- l, y <=x], [y| y <- l, y > x])
 
 
+--4.5 
+data BinTree a = Empty | NodeBT a (BinTree a) (BinTree a)
+		  deriving Show 
+
+treecompare Empty Empty = True 
+treecompare Empty (NodeBT v lf rt) = False
+treecompare (NodeBT v lf rt) Empty = False 
+treecompare (NodeBT v1 lf1 rt1) (NodeBT v2 lf2 rt2) = (((v1==v2) && (treecompare lf1 lf2)) && (treecompare rt1 rt2))
+
+a1 = (NodeBT 3 (NodeBT 2 (NodeBT 3 Empty Empty) (NodeBT 1 Empty Empty)) (NodeBT 2 (NodeBT 1 Empty Empty) (NodeBT 1 Empty Empty)))
+a2 = (NodeBT 3 (NodeBT 2 (NodeBT 1 Empty Empty) (NodeBT 1 Empty Empty)) (NodeBT 2 (NodeBT 1 Empty Empty) (NodeBT 1 Empty Empty)))
+
+--4.7 
+data BinTree'' a = Leaf'' a 
+		|NodeBT'' (BinTree'' a) (BinTree'' a)
+		deriving Show
+
+ff t = (flipT . flipT) t
+flipT (NodeBT'' a b) = NodeBT'' (flipT b) (flipT a)
+flipT (Leaf'' a) = Leaf'' a
+
+a3 = Leaf'' 3
+a4 = NodeBT'' (Leaf'' 1) (Leaf'' 2)
+
+--ff reverses the whole tree
+ff' (NodeBT'' a b) = (NodeBT'' b a)
