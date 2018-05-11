cacc8fc43b6a50abe25495c9ab5e8d8506629d03
JSai23 <23284633+JSai23@users.noreply.github.com>
2018-05-11 18:19:42 -0400

    Add files via upload

--- PQueue.hs
+++ PQueue.hs
@@ -1,9 +1,10 @@
 --ADT for Priority Queue
-module PQueue(PQueue(..),pqEmpty,enPQ,dePQ,frontPQ) where
+module PQueue(PQueue(..),pqEmpty,enPQ,enPQ',dePQ,frontPQ) where
 
 emptyPQ :: PQueue a
 pqEmpty :: PQueue a -> Bool
 enPQ    :: (Ord a) => a -> PQueue a -> PQueue a
+enPQ'   :: (Ord a) => a -> PQueue a -> (a -> a -> Bool) -> PQueue a
 dePQ    :: (Ord a) => PQueue a -> PQueue a
 frontPQ :: (Ord a) => PQueue a -> a
 
@@ -18,7 +19,15 @@ pqEmpty _ 	  = False
 enPQ x (PQ q) 	  = PQ (insert x q) 
 	  where insert x [] 	              = [x]
 		insert x r@(e:r') | x <= e    =x:r
-				  | otherwise =e:insert x r'
+						  | otherwise =e:insert x r'
+--5.2				  
+enPQ' x (PQ q) f = PQ (insert x q f) 
+	where 
+		insert x [] _	                 =[x]
+		insert x r@(e:r') f | f x e          =x:r
+							| otherwise      =e:insert x r' f
+--end of 5.2 			
+			
 dePQ (PQ [])	  = error "dePQ: empty priority queue"
 dePQ (PQ(x:xs))   = PQ xs
 
