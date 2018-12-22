3fff6adeec91d39937ea560ba0cb9cd0e0136b07
JSai23 <23284633+JSai23@users.noreply.github.com>
2018-12-22 14:40:27 -0500

    Add files via upload

--- ch6.hs
+++ ch6.hs
@@ -10,4 +10,40 @@ list2 = [("Aam",3),("Bob",2),("Sam",4)]
 --tuple comparison works
 
 --6.3
-bubblesort [] = []
+bubblesort n [] = []
+bubblesort n xs 
+	| n == 0    = xs
+	| otherwise = bubblesort (n - 1) (bubblesort' xs)
+
+bubblesort' (x:y:xs) 
+	| x > y  = y : bubblesort'(x:xs)
+	| x <= y = x : bubblesort'(y:xs)
+bubblesort' (x) = (x)	
+
+--6.3b
+test2 = [1,5,2,3,4]
+
+bubblesort'' n [] = []
+bubblesort'' n xs 
+	| n == 0                    = xs
+	| (bubblesort''' xs) == xs  = xs
+	| otherwise                 = bubblesort'' (n - 1) (bubblesort''' xs)
+
+bubblesort''' (x:y:xs) 
+	| x > y  = y : bubblesort'''(x:xs)
+	| x <= y = x : bubblesort'''(y:xs)
+bubblesort''' (x) = (x)	
+
+--6.4
+
+--6.5
+qsort n [] = []
+qsort n (pivot:rest) 
+	|(length rest) <= n = (isort rest)
+	|otherwise = 
+		qsort lower ++ [pivot] ++ qsort upper
+		where 
+			lower = [x | x <- rest, x <= pivot]
+			upper = [x | x <- rest, x  > pivot]
+		
+
