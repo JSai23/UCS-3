cfa294ed6c09af09e55f8f13d74b173d903ab55a
JSai23 <23284633+JSai23@users.noreply.github.com>
2019-01-14 18:23:45 -0500

    Add files via upload

--- ch5.hs
+++ ch5.hs
@@ -1,11 +1,12 @@
 --5.1
--- import PQueue
--- import USet
--- import OSet
--- import MSet
--- import TTable
+import PQueue
+import USet
+import OSet
+import MSet
+import TTable
 import Array
-import Heap
+
+
 
 data Point a = Pt (a,a) 
 	deriving Show
@@ -41,6 +42,40 @@ x = array (((0),(0)),((1),(1))) [(((0),(0)),(1)),(((0),(1)),(2)), (((1),(0)),(1)
 test7 = TTbl(x)
 test8 = array (0,3) [(0, 1),(1,2),(2,3),(3,4)]
 test9 = array (0,3) [(0, 1),(1,2),(2,3),(3,4)]
+test10 = [1,2,3]
+test11 = [1,2,3]
+
+--5.7
+data Heap a = EmptyHP | HP a Int (Heap a) (Heap a) deriving Show 
+	
+emptyHeap = EmptyHP
+heapEmpty EmptyHP = True
+heapEmpty _  = False
+
+f indHeap EmptyHP  = error "findHeap : empty heap "
+f indHeap (HP x _ a b) = x 
+
+rank :: (Ord a) => Heap a -> Int
+rank EmptyHP = 0
+rank (HP _ r _ _ ) = r
+
+makeHP :: (Ord a) => a -> Heap a -> Heap a -> Heap a
+makeHP x a b 
+  | (rank a >= rank b) = HP x (rank b + 1) a b
+  | otherwise          = HP x (rank a + 1) b a
+			 
+merge :: (Ord a) => Heap a -> Heap a -> Heap a
+merge h EmptyHP = h
+merge EmptyHP h = h
+merge h1@(HP x _ a1 b1) h2@ (HP y _ a2 b2)
+		| x <= y = makeHP x a1 (merge b1 h2)
+		| otherwise = makeHP y a2 (merge h1 b2)
+		
+insHeap x h = merge (HP x 1 EmptyHP EmptyHP) h 
+
+delHeap EmptyHP = error " delHeap : empty heap "
+delHeap (HP x _ a b) = merge a b 
+
 test10 = [1,2,3]
 test11 = [1,2,3]
 
